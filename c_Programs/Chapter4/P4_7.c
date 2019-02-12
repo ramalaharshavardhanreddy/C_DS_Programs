b4eff904b2d82ea7b32f58a54d173b8e5ea6ef77
ramalaharshavardhanreddy <ramalaharshavardhan@gmail.com>
2019-02-12 12:37:11 +0530

    Updates on comma operator program and swaping of 2 variables in Chapter-4.

--- c_Programs/Chapter4/P4_7.c
+++ c_Programs/Chapter4/P4_7.c
@@ -1,9 +1,79 @@
 /*P4.7 Program to understand the use of comma operator*/
+/* comma operator used in 2 contexts 1) A separator Ex:- int a = 1, 2, 3;
+ *                                   2) An Operator Ex:- a = 1, 2, 3;
+ * --When comma is used as an operator, the operands will be evaluated from 
+ *   LEFT to RIGHT.
+ * -- int var = sum(a(), b()); when comma is used as a separator, the order in 
+ *    which two expressions (separated by comma) are evaluated is NOT DEFINED. 
+ */ 
 #include<stdio.h>
+
+/* Function Prototypes */
+int comma_1(void);
+int comma_2(void); 
+int comma_3(void); 
+int comma_4(void);
+int comma_5(void);
+
 int main(void)
 {
 	int a,b,c,sum;
 	sum = (a=8,b=7,c=9,a+b+c);	
-	printf("Sum=%d\n",sum);
+	printf("Sum=%d\n",sum); 
+    
+    /* Functions to show the various properties of comma operator */  
+    comma_1();
+    comma_2();
+    comma_3();
+    comma_4();
+    comma_5();
+
 	return 0;
+}
+int comma_1(void)
+{
+    /* comma is the last operator in precedence table. So comma should be 
+     * carefully used on right side of an assignment expression. 
+    */
+    int a = 10, b; 
+    b = 20, a;   // b = 20 
+    printf(" b = %d ", b); 
+    getchar(); 
+    return 0; 
+}
+int comma_2(void)
+{
+    /* Putting a bracket with comma makes b = a (or 10). */
+    int a = 10, b; 
+    b = (20, a); // b = a 
+    printf(" b = %d ", b); 
+    getchar(); 
+    return 0; 
+
+}
+int comma_3(void)
+{
+    /* Comma works just as a separator  So compilation Error */
+    int a = 1, 2, 3; 
+    printf("%d", a); 
+    return 0; 
+}
+int comma_4(void)
+{
+    /* Comma works as an operator. Precedence of comma operator is least in 
+     * operator precedence table. So the assignment operator takes precedence 
+     * over comma and the expression a = 1, 2, 3; becomes equivalent to 
+     * (a = 1), 2, 3
+     * */
+    int a; 
+    a = 1, 2, 3; 
+    printf("%d", a); //a = 1;
+    return 0; 
+}
+int comma_5(void)
+{
+    /* The result of comma operator cannot be used as l-value in c  */
+    int x=5, y=10
+    (x, y) = 20;
+    printf("%d", y);  // Undefined in C-Language
 }
--- c_Programs/Chapter4/P4_8.c
+++ c_Programs/Chapter4/P4_8.c
@@ -1,10 +1,62 @@
 /*P4.8 Program to interchange the value of two variables using comma operator*/
 #include<stdio.h>
+
+/* Function Prototypes */ 
+int Swap_using_operators1(void);
+int Swap_using_operators2(void);
+int Swap_using_operators3(void);
+
 int main(void)
 {
  	int a=8,b=7,temp;
 	printf("a=%d, b=%d\n",a,b);
 	temp=a,a=b,b=temp;
 	printf("a=%d, b=%d\n",a,b);
+
+    /* Various ways of Swaping */ 
+    Swap_using_operators1();   /* Using +,-    */
+    Swap_using_operators2();   /* Using *,/    */
+    Swap_using_operators3();   /* Using Exor   */ 
+
 	return 0;
+}
+int Swap_using_operators1(void)
+{
+    int a=10,b=20;
+    printf("\n Before Swaping a=%d,b=%d \n",a,b);
+    a=a+b;  // a=10+20 :: a=30 & b=20
+    b=a-b;  // b=30-20 :: b=10 & a=30
+    a=a-b;  // a=30-10 :: a=20 & b=10 
+    printf("\n After Swaping a=%d,b=%d \n",a,b);
+    return 0;
+}
+int Swap_using_operators2(void)
+{
+
+    int a=10,b=20;
+    printf("\n Before Swaping a=%d,b=%d \n",a,b);
+    a=a*b;  // a=10*20 :: a=200 & b=20
+    b=a/b;  // b=200/20 :: b=10 & a=200
+    a=a/b;  // a=200/10 :: a=20 & b=10 
+    printf("\n After Swaping a=%d,b=%d \n",a,b);
+    return 0;
+}
+int Swap_using_operators3(void)
+{
+    /*  0 XOR 0 = 0
+     *  0 XOR 1 = 1
+     *  1 XOR 0 = 1
+     *  1 XOR 1 = 0 
+     *  a XOR 0 = a
+     *  a XOR 1 = a-bar
+     *  a XOR a = 0
+     */
+    int a=10,b=20;
+    printf("\n Before Swaping a=%d,b=%d \n",a,b);
+    a=a^b;  // a=10^20 :: a=30 & b=20
+    b=a^b;  // b=30^20 :: b=10 & a=30
+    a=a^b;  // a=30^10 :: a=20 & b=10 
+    printf("\n After Swaping a=%d,b=%d \n",a,b);
+    return 0;
+
 }
